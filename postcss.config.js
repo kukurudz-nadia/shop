54ee455b7f44ad635756d4f51fe1c671d0546891
Nadia <kukurudz.nadia6@gmail.com>
2023-01-20 15:14:33 +0200

    added footer and header, gem webpacker

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*',
-  ],
-  theme: {
-    extend: {},
-  },
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require("tailwindcss"),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
