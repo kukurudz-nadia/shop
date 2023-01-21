85c09c01a62dc13807e6156b9da363f021321443
Nadia <kukurudz.nadia6@gmail.com>
2023-01-22 00:22:05 +0200

    added products seeds, controller, views

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,50 @@
+class ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(quote_params)
+
+    if @product.save
+      redirect_to products_path, notice: "Product was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: "Product was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: "Product was successfully destroyed."
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :position, :category_id)
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  validates :price, :name, presence: true
+  validates :price, numericality: { greater_than: 0}
+
   belongs_to :category
   has_and_belongs_to_many :orders
 end
