dd85339fa90f76cb2b64de2c429d8b3d2e02a467
Nadia <kukurudz.nadia6@gmail.com>
2023-01-23 22:32:27 +0200

    added categories show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,29 @@ class CategoriesController < ApplicationController
     end
     
     def show
+        @category = Category.find(params[:id])
+    end
+    
+    def show_1_category
+    end
+    
+    def show_2_category
+    end
+
+    def show_3_category
+    end
+    def show_4_category
+    end
+    def show_5_category
+    end
+    def show_6_category
+    end
+    def show_7_category
+    end
+    def show_8_category
+    end
+    def show_9_category
+    end
+    def show_10_category
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,16 @@ Rails.application.routes.draw do
   delete 'carts_products/:id' => "carts_products#destroy"
   post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
   delete "products/remove_from_cart/:id", to: "products#remove_from_cart", as: "remove_from_cart"
+  get 'categories/show_1_category' => "categories#show_1_category"
+  get 'categories/show_2_category' => "categories#show_2_category"
+  get 'categories/show_3_category' => "categories#show_3_category"
+  get 'categories/show_4_category' => "categories#show_4_category"
+  get 'categories/show_5_category' => "categories#show_5_category"
+  get 'categories/show_6_category' => "categories#show_6_category"
+  get 'categories/show_7_category' => "categories#show_7_category"
+  get 'categories/show_8_category' => "categories#show_8_category"
+  get 'categories/show_9_category' => "categories#show_9_category"
+  get 'categories/show_10_category' => "categories#show_10_category"
   resources :categories
   resources :products
   resources :orders
