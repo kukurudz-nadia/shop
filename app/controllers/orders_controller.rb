b6aadceef926c11aaf7bf4a9483cd590469ff8a1
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-02-14 07:32:11 +0200

    added a part of order (#38)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,27 +4,53 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order = Order.new
+    @order = Order.create(user_id: current_user.id, status: "created", ordered_at: DateTime.current)
+    current_user.cart.products.each do |item|
+      @order.products << item
+      current_user.cart[:id] = nil
+    end
+    # address = Address.new(address_params)
+    # address.save
+    # @order_detail = OrderDetail.new(order_detail_params)
+    # @order_detail.save
+    @order.save
   end
 
   def create
     @order = Order.new(order_params)
+   
+    current_user.cart.cart_products.each do |item|
+      @order.cart_products << item
+      item.cart_id = nil
+    end
+    @order.save
+    Cart.destroy(session[:cart_id])
+    session[:cart_id] = nil
+    redirect_to root_path
+  end
 
-    if @order.save
-      redirect_to orders_path, notice: "Order was successfully created."
-    else
-      render :new
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to @order, notice: 'Website was successfully updated.' }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
     end
   end
 
-  def destroy
-    @order.destroy
-    redirect_to orders_path, notice: "Order was successfully destroyed."
-  end
-
   private
+    def order_params
+      params.require(:order).permit()
+    end
 
-  def order_params
-    params.require(:order).permit(:status, :ordered_at)
-  end
+    # def order_detail_params
+    #   params.require(:order_detail).permit(:first_name, :last_name, :email, address_id: address.id, order_id: @order.id)
+    # end
+
+    # def address_params
+    #   params.require(:address).permit(:street, :city, :coutry, :comment, user_id: current_user.id)
+    # end
 end
